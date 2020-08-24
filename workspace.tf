65268dd81b148fd73942bdbb593eae530a13bc6a
vinnie <vinnie.mazza@gmail.com>
2020-08-24 11:54:46 -0400

    update to 20.04

--- workspace.tf
+++ workspace.tf
@@ -64,7 +64,7 @@ resource "azurerm_virtual_machine" "workspace" {
     storage_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
+        sku       = "20.04-LTS"
         version   = "latest"
     }
 
